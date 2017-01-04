de9e2e67b715d41834fd8c9bff59869afc283fe5
Damián García <damian.garcia@zalando.de>
2017-01-04 13:37:12 +0100

    feat(images): add nicer images

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 
-import logo from '../../assets/images/logo.svg';
+import './header.scss';
+import logo from '../../assets/images/logo.png';
 
 class Header extends Component {
   render() {
     return (
         <div className="header">
-          <img src={logo} className="logo" alt="logo"/>
-          <h2>Beer collection</h2>
+          <div className="logo">
+            <img src={logo} alt="logo"/>
+          </div>
+          <span className="title">Beer<br/>collection</span>
         </div>
     );
   }
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,12 +2,23 @@ import React, {Component} from 'react';
 import './dashboard.scss';
 import DashboardOption from './DashboardOption';
 
+import crate from '../../assets/images/crate.png';
+import tap from '../../assets/images/tap.png';
+
 class Dashboard extends Component {
   render() {
     return (
         <div className="dashboard">
-          <DashboardOption title="Add beer to the list" url="/beer/add"/>
-          <DashboardOption title="See list of beers" url="/beer/list"/>
+          <DashboardOption
+              title="Add beer to the list"
+              url="/beer/add"
+              image={tap}
+          />
+          <DashboardOption
+              title="See list of beers"
+              url="/beer/list"
+              image={crate}
+          />
         </div>
     );
   }
