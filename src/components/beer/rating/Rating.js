06ff583d2e8e2eb02ad74eb39a6e9ea12d04696d
Damián García <damian.garcia@zalando.de>
2017-04-20 17:43:52 +0200

    feat(rating): add rating component

--- src/components/beer/rating/Rating.js
+++ src/components/beer/rating/Rating.js
@@ -3,12 +3,21 @@ import React from 'react';
 import './rating.scss';
 
 const Rating = () => (
-    <div className="rating">
-      <i className="fa fa-heart-o"></i>
-      <i className="fa fa-star-o"></i>
-      <i className="fa fa-smile-o"></i>
-      <i className="fa fa-meh-o"></i>
-      <i className="fa fa-frown-o"></i>
+    <div className="rating form-group">
+      <input type="radio" id="rating-5" name="rating" className="rating__input"></input>
+      <label htmlFor="rating-5" className="fa fa-heart-o rating__label"></label>
+
+      <input type="radio" id="rating-4" name="rating" className="rating__input"></input>
+      <label htmlFor="rating-4" className="fa fa-star-o rating__label"></label>
+
+      <input type="radio" id="rating-3" name="rating" className="rating__input"></input>
+      <label htmlFor="rating-3" className="fa fa-smile-o rating__label"></label>
+
+      <input type="radio" id="rating-2" name="rating" className="rating__input"></input>
+      <label htmlFor="rating-2" className="fa fa-meh-o rating__label"></label>
+
+      <input type="radio" id="rating-1" name="rating" className="rating__input"></input>
+      <label htmlFor="rating-1" className="fa fa-frown-o rating__label"></label>
     </div>
 );
 
