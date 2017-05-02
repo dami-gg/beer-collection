c953df7fd5b4eca794625a972a3a7b6c0ffba013
Damián García <damian.garcia@zalando.de>
2017-05-02 17:21:15 +0200

    style(dashboard): improve style of dashboard options

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,13 +7,15 @@ import './dashboard.scss';
 
 import crate from '../../assets/images/crate.png';
 import tap from '../../assets/images/tap.png';
+import statistics from '../../assets/images/statistics.png';
+import globe from '../../assets/images/globe.png';
 
 class Dashboard extends Component {
   render() {
     return (
         <div className="dashboard">
           <DashboardOption
-              title="Add beer to your collection"
+              title="Add a new beer"
               url="/beer/add"
               image={tap}/>
 
@@ -21,6 +23,16 @@ class Dashboard extends Component {
               title="See your collection"
               url="/collection"
               image={crate}/>
+
+          <DashboardOption
+              title="See statistics"
+              url="/statistics"
+              image={statistics}/>
+
+          <DashboardOption
+              title="Beer map"
+              url="/map"
+              image={globe}/>
         </div>
     );
   }
--- src/components/dashboard/dashboard-option/DashboardOption.js
+++ src/components/dashboard/dashboard-option/DashboardOption.js
@@ -3,19 +3,25 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
+import './dashboard-option.scss';
+
 class DashboardOption extends Component {
-  props:{
+  props: {
+    title: string,
     url: string,
-    image: string,
-    title: string
+    image: string
   };
 
   render() {
     return (
         <div className="dashboard__option">
-          <Link to={this.props.url}>
-            <img src={this.props.image} className="dashboard__option__image" alt="logo"/>
-            <p className="dashboard__option__title">{this.props.title}</p>
+          <Link className="dashboard__option__card" to={this.props.url}>
+            <div className="dashboard__option__image-wrapper">
+              <img src={this.props.image} className="dashboard__option__image" alt="logo"/>
+            </div>
+            <div className="dashboard__option__title-wrapper">
+              <p className="dashboard__option__title">{this.props.title}</p>
+            </div>
           </Link>
         </div>
     );
