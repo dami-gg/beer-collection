bff965fa67661e6a44a3699444b82cb5c8bf4429
Damián García <damian.garcia@zalando.de>
2017-05-03 10:15:36 +0200

    feat(collection): insert newly created beers keeping alphabetical order

--- src/reducers/collection.reducers.js
+++ src/reducers/collection.reducers.js
@@ -10,10 +10,28 @@ const collection = (state: Array<Beer> = initialState, action: Object): Array<Be
 
   switch (action.type) {
     case actionTypes.ADD_BEER_TO_STATE:
-      return [
-        ...state,
-        action.beer
-      ];
+      // If the beer natural position by alphabetical order is not the last, which would mean it's added on startup from
+      // the database, it means it's been created in the current session and has to be inserted in the right position
+      debugger;
+      const lastBeer = state.length > 0 ? state[state.length - 1] : null;
+      const newBeerName = action.beer.name.toLowerCase();
+
+      if (lastBeer && newBeerName < lastBeer.name.toLowerCase()) {
+        index = state.findIndex(element => element.name.toLowerCase() > newBeerName);
+
+        return [
+          ...state.slice(0, index),
+          action.beer,
+          ...state.slice(index + 1)
+        ];
+      }
+
+      else {
+        return [
+          ...state,
+          action.beer
+        ];
+      }
 
     case actionTypes.UPDATE_BEER_IN_STATE:
       index = state.findIndex(element => element.id === action.beer.id);
@@ -23,7 +41,6 @@ const collection = (state: Array<Beer> = initialState, action: Object): Array<Be
         action.beer,
         ...state.slice(index + 1)
       ];
-
 
     case actionTypes.DELETE_BEER_FROM_STATE:
       index = state.findIndex(element => element.id === action.beer.id);
