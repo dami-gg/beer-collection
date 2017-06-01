5a013c7648b677e815433dbacf46f21ce17bfbd6
Damián García <damian.garcia@zalando.de>
2017-06-01 12:27:31 +0200

    chore(webpack): hot module reloading

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import firebase from 'firebase';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import firebase from "firebase";
+import { AppContainer } from "react-hot-loader";
 
-import {createStore, applyMiddleware} from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
 
-import reducers from './reducers';
-import rootSaga  from './sagas';
+import reducers from "./reducers";
+import rootSaga from "./sagas";
 
-import App from './components/app/App';
+import App from "./components/app/App";
 
-import './assets/styles/main.scss';
+import "./assets/styles/main.scss";
 
 const config = {
   apiKey: "AIzaSyAxomPcdjUYjos_6-Vcjj07sazCVpoQBHE",
@@ -25,17 +26,29 @@ firebase.initializeApp(config);
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(
-    reducers,
-    applyMiddleware(sagaMiddleware)
-);
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
 
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
+  <AppContainer>
     <Provider store={store}>
       <App />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
+  </AppContainer>,
+  document.getElementById("root")
 );
 
+if (module.hot) {
+  module.hot.accept("./components/app/App", () => {
+    const NextApp = require("./components/app/App").default; // eslint-disable-line global-require
+    ReactDOM.render(
+      <AppContainer>
+        <Provider store={store}>
+          <NextApp />
+        </Provider>
+      </AppContainer>,
+      document.getElementById("root")
+    );
+  });
+}
